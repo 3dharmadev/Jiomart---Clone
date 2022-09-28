332a2d2c49b992426b85eca63ab788bc671217b2
anki2001ta <ankitadeb1202@gmail.com>
2022-09-28 23:51:18 +0530

    footer added

---
+++ footer.js
@@ -0,0 +1,59 @@
+function footer(){
+    return `          <div id="footer-div">
+    <div>
+        <h2>MOST POPULAR CATEGORIES </h2>
+        <a href="#"><p>Staples</p></a>
+        <a href="#"><p>Beverages</p></a>
+        <a href="#"><p>Personal Care</p></a>
+        <a href="#"><p>Home Care</p></a>
+        <a href="#"><p>Fruits and Vegetables</p></a>
+        <a href="#"><p>Baby Care</p></a>
+        <a href="#"><p>Snacks & Branded Foods</p></a>
+        <a href="#"><p>Dairy & Bakery</p></a>
+    </div>
+   
+    <div id="ft">
+    <h2>CUSTOMER SERVICES</h2>
+    <a href="#"><p>About Us</p></a>
+    <a href="#"><p>FAQ</p></a>
+    <a href="#"><p>Terms and conditions</p></a>
+    <a href="#"><p>Privacy policy</p></a>
+    <a href="#"><p>E-waste Policy</p></a>
+    <a href="#"><p> Cancellation and return policy</p></a> 
+    </div>
+    <div>
+    </div>
+    <div id="f">
+        <h2>CONTACT US</h2>
+        <p>WhatsApp us :<label class="lbl"> 70003 70003 </label></p>
+        <p>Call Us :<label class="lbl"> 1800 890 1222 8:00 AM to 8:00 PM, 365 days</p>
+        <p>Please note that you are accessing the BETA Version of <label class="lbl">www.jiomart.com</p>
+        <p>Should you encounter any bugs, glitches, lack of functionality, delayed deliveries, billing errors or other problems on the beta website, please email us on <label class="lbl">cs@jiomart.com</p>
+        <br>
+
+        <h5>DOWNLOAD APP</h5>
+        <div id="pic">
+        <img src="https://web-static.wrike.com/tp/static/assets/img/footer/google_play_en.svg">
+        <img src="https://web-static.wrike.com/tp/static/assets/img/footer/google_play_en.svg">
+        </div>
+    </div>
+  
+                </div>
+                <div id="footer-bottom">
+                    <p></p>
+<hr>          
+    <div id="dis"><p style="text-align:left;">Best viewed on Microsoft Edge 81+, Mozilla Firefox 75+, Safari 5.1.5+, Google Chrome 80+ </p></div>         
+       </div>`
+ }
+export {footer}
+
+
+
+
+
+
+
+
+
+
+
